ab8f0d57ab201c9645f6a0e4679db38388aaa0ee
alvinseyidov <alvinseyidov@gmail.com>
2023-08-27 01:18:28 +0400

    update

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def index(request):
     topics = HotTopics.objects.all()
     general = General.objects.all()
     blogs = Blog.objects.all()
-    tags = Tag.objects.all()
+    tags = Tag.objects.all()[:6]
     socials = Social.objects.all()
     context = {
         "tags": tags,
