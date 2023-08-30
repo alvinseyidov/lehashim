f08ef0c97ff6793f88cfba08e68a39df4770eac2
alvinseyidov <alvinseyidov@gmail.com>
2023-08-30 20:48:05 +0400

    update

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Blog)
-admin.site.register(BlogCategory)
 admin.site.register(Tag)
 
 
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    model = BlogCategory
+    list_display = ['name','parent']
