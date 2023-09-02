2279c5209c277e6ff29b18fec5bf8195720c92a9
alvinseyidov <alvinseyidov@gmail.com>
2023-09-02 22:58:59 +0400

    update

--- blog/models.py
+++ blog/models.py
@@ -26,6 +26,7 @@ class BlogCategory(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=256)
+    reading_time = models.CharField(max_length=256)
     main_image = models.ImageField()
     short_description = models.TextField()
     description = models.TextField()
