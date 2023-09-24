8670309593f5532a9fa5b82837000b0271104e76
alvinseyidov <alvinseyidov@gmail.com>
2023-09-24 18:22:35 +0400

    updatE

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
 from core.models import *
-from blog.models import BlogCategory as Category, Tag
-
+from blog.models import BlogCategory as Category, Blog, Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def category(request, id):
     categories = Category.objects.filter(parent__isnull=True)
     category = Category.objects.get(pk=id)
     general = General.objects.all()
     socials = Social.objects.all()
+
+    categories = Category.objects.filter(parent__isnull=True)
+    featured = Featured.objects.all()
+    topics = HotTopics.objects.all()
+    general = General.objects.all()
+    blogs = Blog.objects.all()
+    blogsf = Blog.objects.all()[:3]
+    blogsfmobile = Blog.objects.all()[:1]
+    tags = Tag.objects.all()[:6]
+    socials = Social.objects.all()
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(blogs, 2)
+    try:
+        blgs = paginator.page(page)
+    except PageNotAnInteger:
+        blgs = paginator.page(1)
+    except EmptyPage:
+        blgs = paginator.page(paginator.num_pages)
     context = {
-        "general": general,
         "category": category,
+        "tags": tags,
+        "blogs": blgs,
+        "blogsf": blogsf,
+        "blogsfmobile": blogsfmobile,
+        "topics": topics,
+        "general": general,
+        "featured": featured,
         "socials": socials,
         "categories": categories
     }
