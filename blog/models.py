b08a450755ad72a53ae8977c8668073ce71e5717
alvinseyidov <alvinseyidov@gmail.com>
2023-10-31 14:42:19 +0400

    update

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class Blog(models.Model):
     main_image = models.ImageField()
     short_description = models.TextField()
     description = models.TextField()
+    category = models.ForeignKey(BlogCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name="blogs")
     tags = models.ManyToManyField(Tag, blank=True, related_name="blogs")
 
     def __str__(self):
