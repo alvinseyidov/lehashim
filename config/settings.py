8666a36fcb626a91cc80a06847d8d423028dd7e8
Elvin Seyidov <alvinseyidov@gmail.com>
2024-02-16 17:39:11 +0400

    update

--- config/settings.py
+++ config/settings.py
@@ -181,9 +181,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'az'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Baku'
 
 USE_I18N = True
 
